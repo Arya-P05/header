ff06e5e872972b21a8048f9bd6492f2412cca65b
Arya Patel <97847987+Arya-P05@users.noreply.github.com>
2024-01-19 20:18:51 -0500

    Update and rename H.js to Header.js

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+// packages/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Reusable Header</h1>
+    </header>
+  );
+};
+
+export default Header;
