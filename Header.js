f6bcd9d370c53cb0dbaea336a4a718f463f888e7
Arya Patel <97847987+Arya-P05@users.noreply.github.com>
2024-01-19 23:07:05 -0500

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,42 +1,18 @@
-// components/Header.js
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledHeader = styled.header`
-  background-color: #333;
-  color: #fff;
-  padding: 1rem;
-  text-align: center;
-  font-family: 'YourChosenFont', sans-serif;
-`;
-
-const Navigation = styled.nav`
-  margin-top: 10px;
-`;
-
-const NavLink = styled.a`
-  color: #fff;
-  text-decoration: none;
-  margin: 0 15px;
-  font-size: 1rem;
-  transition: color 0.3s ease;
-
-  &:hover {
-    color: #ffcc00; /* Change the color on hover */
-  }
-`;
 
 const Header = () => {
   return (
-    <StyledHeader>
-      <h1>My Reusable Header</h1>
-      <Navigation>
-        <NavLink href="#">Home</NavLink>
-        <NavLink href="#">About</NavLink>
-        <NavLink href="#">Services</NavLink>
-        <NavLink href="#">Contact</NavLink>
-      </Navigation>
-    </StyledHeader>
+    <header>
+      <h1>Header Component</h1>
+      <p>Reusable within your Next.js Projects</p>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
