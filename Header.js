640a38282fe22b07ca122045569332b0ef77ad8d
Arya Patel <97847987+Arya-P05@users.noreply.github.com>
2024-01-19 22:23:06 -0500

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,45 +1,11 @@
-// components/Header.js
+// Header.js
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledHeader = styled.header`
-  background-color: #333;
-  color: #fff;
-  padding: 1rem;
-  text-align: left;
-  font-family: 'YourChosenFont', sans-serif;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Navigation = styled.nav`
-  margin-top: 10px;
-`;
-
-const NavLink = styled.a`
-  color: #fff;
-  text-decoration: none;
-  margin: 0 15px;
-  font-size: 1rem;
-  transition: color 0.3s ease;
-
-  &:hover {
-    color: #ffcc00; /* Change the color on hover */
-  }
-`;
 
 const Header = () => {
   return (
-    <StyledHeader>
-      <h1>My Reusable Header</h1>
-      <Navigation>
-        <NavLink href="#">Home</NavLink>
-        <NavLink href="#">About</NavLink>
-        <NavLink href="#">Services</NavLink>
-        <NavLink href="#">Contact</NavLink>
-      </Navigation>
-    </StyledHeader>
+    <header>
+      <h1>NO FUCKING WAY</h1>
+    </header>
   );
 };
 
